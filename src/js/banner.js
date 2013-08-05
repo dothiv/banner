f928539d574e61328796cfefec4deac3cf43adad
Benedikt Budig <benedikt.budig@binaerpiloten.de>
2013-08-05 13:30:00 +0200

    fix(IE): fix center banner positioning in IE6 standard mode

--- src/js/banner.js
+++ src/js/banner.js
@@ -190,8 +190,15 @@
         if(navigator.appName.indexOf("Internet Explorer")!=-1 && document.compatMode!=='CSS1Compat')
             bannerContainer.style.position = 'absolute';
 
-        outerContainer.appendChild(bannerContainer);
+        // Specials for IE6 standard mode
+        var msie = parseInt((/msie (\d+)/.exec(navigator.userAgent.toLowerCase()) || [])[1], 10);
+        if (msie < 6 && document.compatMode=='CSS1Compat') {
+            bannerContainer.style.position = 'absolute';
+            bannerBackground.style.width = 1200px;
+        }
+
         outerContainer.appendChild(bannerBackground);
+        outerContainer.appendChild(bannerContainer);
 
         document.body.insertBefore(outerContainer, document.body.firstChild);
 
